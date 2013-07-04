f6f6d78fbe939ad1d9bc40d9a476f580826bcef1
Nick Douma <n.douma@nekoconeko.nl>
2013-07-04 13:29:12 +0200

    Attach LICENSE to all files

--- src/nl/cyso/vcloud/client/config/Configuration.java
+++ src/nl/cyso/vcloud/client/config/Configuration.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright (c) 2013 Cyso < development [at] cyso . nl >
+ *
+ * This file is part of vcloud-client.
+ *
+ * vcloud-client is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * vcloud-client is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with vcloud-client. If not, see <http://www.gnu.org/licenses/>.
+ */
 package nl.cyso.vcloud.client.config;
 
 import java.math.BigInteger;
